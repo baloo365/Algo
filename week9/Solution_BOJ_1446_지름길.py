2bd012697ae9de97be30ad679d5ea0d78c10eabf
baloo365 <124110711+baloo365@users.noreply.github.com>
2023-10-28 23:23:05 +0900

    Update Solution_BOJ_1446_지름길

--- week9/Solution_BOJ_1446_지름길.py
+++ week9/Solution_BOJ_1446_지름길.py
@@ -1,1 +1,27 @@
-Add week9 Solution_BOJ_1446_지름길
+import sys
+
+N, D = map(int, input().split())
+
+
+def shortcut():
+    global length
+    global s
+    global e
+    distance = list(range(D+1))
+
+    for i in range(D+1):
+        distance[i] = min(distance[i], distance[i - 1] + 1)
+        for s, e, length in arr:
+            if e > D:
+                continue
+            if distance[e] > distance[s] + length:
+                distance[e] = distance[s] + length
+    return distance[-1]
+
+arr = [[] for _ in range(N)]
+for i in range(N):
+    s, e, length = map(int, input().split())
+    arr[i] = (s, e, length)
+
+
+print(shortcut())
