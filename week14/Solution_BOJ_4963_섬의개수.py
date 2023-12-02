5e005e5802eda3b2b91ec8f16a7fb001d36b9f75
baloo365 <124110711+baloo365@users.noreply.github.com>
2023-12-03 00:04:59 +0900

    Update Solution_BOJ_4963_섬의개수.py

--- week14/Solution_BOJ_4963_섬의개수.py
+++ week14/Solution_BOJ_4963_섬의개수.py
@@ -1,15 +1,20 @@
 import sys
 from collections import deque
 
-def bfs():
+def bfs(i, j):
+    dj = [1, -1, 0, 0, 1, -1, 1, -1]
+    di = [0, 0, -1, 1, -1, 1, 1, -1]
+    island[i][j] = 0
+    q = deque()
+    q.append((i, j))
     while q:
         si, sj = q.popleft()
-        island[si][sj] = 0
-        for di, dj in ((0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)):
-            ni, nj = si + di, sj + dj
+        for i in range(8):
+            nj = sj + dj[i]
+            ni = si + di[i]
             # print('nice')
             if 0 <= ni < h and 0 <= nj < w and island[ni][nj] == 1:
-                island[si][sj] = 0
+                island[ni][nj] = 0
                 q.append((ni, nj))
 
 while True:
@@ -18,17 +23,10 @@ while True:
         break
     island =[list(map(int, input().split())) for _ in range(h)]
 
-    # print(w, h, island)
-    visited = [[0] * w for _ in range(h)]
     cnt = 0
     for i in range(h):
         for j in range(w):
             if island[i][j] == 1:
-                island[i][j] = 0
-                q = deque()
-                q.append((i, j))
-                bfs()
+                bfs(i, j)
                 cnt += 1
     print(cnt)
-
-
