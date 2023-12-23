7083083895f6fadc2e5d507b0ae685690bb65949
baloo365 <124110711+baloo365@users.noreply.github.com>
2023-12-23 23:21:53 +0900

    Update Solution_BOJ_2346_풍선터뜨리기.py

--- week17/Solution_BOJ_2346_풍선터뜨리기.py
+++ week17/Solution_BOJ_2346_풍선터뜨리기.py
@@ -1,1 +1,21 @@
-Add week17 Solution_BOJ_2346_풍선터뜨리기
+import sys
+
+N = int(input())
+arr = list(map(int,input().split()))
+index = list(range(1, N+1))
+ans = []
+
+s = 0
+temp = arr.pop(s)
+ans.append(index.pop(s))
+
+while arr:
+    if temp < 0:
+        s = (s+temp)%len(arr)
+    else:
+        s = (s+temp-1)%len(arr)
+    temp = arr.pop(s)
+    ans.append(index.pop(s))
+
+for i in ans:
+    print(i, end=' ')
